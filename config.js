ee7fe31efcba61c9ed3e6be669b88ef686f20bbc
Nicolash44 <118366957+Nicolash44@users.noreply.github.com>
2026-08-15 17:27:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Reemplazá con los datos de tu proyecto Supabase
 // Los encontrás en: Project Settings → API
-const SUPABASE_URL = "https://ocicvmttyqndsrpnnugy.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ocicvmttyqndsrpnnugy.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jaWN2bXR0eXFuZHNycG5udWd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4MTM2NDIsImV4cCI6MjEwMjM4OTY0Mn0.XOrkO4lGgwuL8j2GArpPQJGC6rms7221b9M5Es0iiBI";
